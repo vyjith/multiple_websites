70183e36025e675628a92d724cf9221404a2a51c
Vyjith <38276429+vyjith@users.noreply.github.com>
2021-11-21 05:33:08 +0530

    Update multiple_website.sh

--- multiple_website.sh
+++ multiple_website.sh
@@ -5,6 +5,7 @@ if [ "$EUID" == 0 ]
 then
         w_install
 else
+        echo "Please run this script on root privilege, plese switch the user to root using the sudo su -. Thank you"
         echo "I am exiting from here now"
         exit
 fi
@@ -22,9 +23,11 @@ case $Answer in
         ;;
         2)
                 echo "You have selected two websites. Please hold a moment, while creating two website for you"
+                w_twowebsite
         ;;
         3)
                 echo "You have selected three websites. Please hold a moment, while creating two website for you"
+                w_threewebsite
         ;;
         *)
                 echo "You have entered invalid option"
@@ -52,6 +55,94 @@ EOF
 echo "Restart httpd service and you can check the website on this URL: http://$domainone after adding the hosts file on your local machine"
 service httpd restart
 echo "The  website is ready and please visit the URL : http://$domainone"
+}
+w_twowebsite(){
+
+echo "Installing httpd service"
+        yum install httpd -y
+        service httpd restart
+echo "Please enter the first website name"
+read -p "NOTE: Use lowercase letter and don't use symbols like($,/,*,&,......):" domainfirst
+mkdir -p /var/www/html/$domainfirst
+cat > /var/www/html/$domainfirst/index.html << EOF
+Hello World!
+EOF
+echo "Please hold a moment"
+cat > /etc/httpd/conf.d/$domainfirst.conf << EOF
+<VirtualHost *:80>
+DocumentRoot /var/www/html/$domainfirst
+ServerName $domainfirst
+ServerAlias $domainfirst
+</VirtualHost>
+EOF
+echo "Please enter the second website name"
+read -p "NOTE: Use lowercase letter and don't use symbols like($,/,*,&,......):" domainsecond
+mkdir -p /var/www/html/$domainsecond
+cat > /var/www/html/$domainsecond/index.html << EOF
+Hello World!
+EOF
+echo "Please hold a moment"
+cat > /etc/httpd/conf.d/$domainsecond.conf << EOF
+<VirtualHost *:80>
+DocumentRoot /var/www/html/$domainsecond
+ServerName $domainsecond
+ServerAlias $domainsecond
+</VirtualHost>
+EOF
+echo "Restart httpd service and you can check the website on this URL: http://$domainfirst &&  http://$domainsecond after adding the hosts file on your local machine"
+service httpd restart
+echo "The  website is ready and please visit the URL : http://$domainfirst && http://$domainsecond"
+}
+
+w_threewebsite(){
+echo "Installing httpd service"
+        yum install httpd -y
+        service httpd restart
+echo "Please enter the first website name"
+read -p "NOTE: Use lowercase letter and don't use symbols like($,/,*,&,......):" domainfirst
+mkdir -p /var/www/html/$domainfirsts
+cat > /var/www/html/$domainfirsts/index.html << EOF
+Hello World!
+EOF
+echo "Please hold a moment"
+cat > /etc/httpd/conf.d/$domainfirsts.conf << EOF
+<VirtualHost *:80>
+DocumentRoot /var/www/html/$domainfirsts
+ServerName $domainfirsts
+ServerAlias $domainfirsts
+</VirtualHost>
+EOF
+echo "Please enter the second website name"
+read -p "NOTE: Use lowercase letter and don't use symbols like($,/,*,&,......):" domainseconds
+mkdir -p /var/www/html/$domainseconds
+cat > /var/www/html/$domainseconds/index.html << EOF
+Hello World!
+EOF
+echo "Please hold a moment"
+cat > /etc/httpd/conf.d/$domainseconds.conf << EOF
+<VirtualHost *:80>
+DocumentRoot /var/www/html/$domainseconds
+ServerName $domainseconds
+ServerAlias $domainseconds
+</VirtualHost>
+EOF
+echo "Please enter the third website name"
+read -p "NOTE: Use lowercase letter and don't use symbols like($,/,*,&,......):" domainthird
+mkdir -p /var/www/html/$domainthird
+cat > /var/www/html/$domainthird/index.html << EOF
+Hello World!
+EOF
+echo "Please hold a moment"
+cat > /etc/httpd/conf.d/$domainthird.conf << EOF
+<VirtualHost *:80>
+DocumentRoot /var/www/html/$domainthird
+ServerName $domainthird
+ServerAlias $domainthird
+</VirtualHost>
+EOF
+echo "Restart httpd service and you can check the website on this URL: http://$domainfirsts &&  http://$domainseconds && http://$domainthird after adding the hosts file on your local machine"
+service httpd restart
+echo "The  website is ready and please visit the URL : http://$domainfirsts && http://$domainseconds && http://$domainthird"
 }
 
 w_main(){
